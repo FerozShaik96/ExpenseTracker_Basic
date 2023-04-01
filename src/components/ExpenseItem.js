d9d740e95de9bf43878b52fb5f068efba3ca6148
FerozShaik96 <ferozshaik1997@gmail.com>
2023-04-01 11:58:08 +0530

    using Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
-function ExpenseItem() {
-  return <h1>Expense Items..!</h1>;
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div>{props.location}</div>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
