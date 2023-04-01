c40563e63f9bd45229bca8fb609710d1072b7af5
FerozShaik96 <ferozshaik1997@gmail.com>
2023-04-01 13:13:42 +0530

    Splited the code into different components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import ExpenseDetails from "./expenseDetails";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div>{props.location}</div>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
     </div>
   );
 }
