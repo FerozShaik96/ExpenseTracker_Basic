5ecbebf8a373e6319c11bb8bfd535d3662820d11
FerozShaik96 <ferozshaik1997@gmail.com>
2023-05-11 17:04:42 +0530

    Adding A new button for Rendering The Form

--- src/components/NewExpence/ExpenseForm.js
+++ src/components/NewExpence/ExpenseForm.js
@@ -120,7 +120,11 @@ const ExpenseForm = (props) => {
           />
         </div>
       </div>
+
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expence</button>
       </div>
     </form>
--- src/components/NewExpence/NewExpenses.js
+++ src/components/NewExpence/NewExpenses.js
@@ -1,16 +1,31 @@
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 function NewExpense(props) {
+  const [isEditing, setEditing] = useState(false);
   const saveExpensesDataHandler = (enteredExpensesData) => {
     const expensesData = {
       ...enteredExpensesData,
       id: Math.random().toString(),
     };
     props.onAddExpenses(expensesData);
+    setEditing(false);
+  };
+  const startEditingHandler = () => {
+    setEditing(true);
+  };
+  const stopEditinghandler = () => {
+    setEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenses={saveExpensesDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}>Add Expense</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenses={saveExpensesDataHandler}
+          onCancel={stopEditinghandler}
+        />
+      )}
     </div>
   );
 }
